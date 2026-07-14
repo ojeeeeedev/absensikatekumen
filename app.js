3e4cff9dfa9fb75b9793a7353ddedf4cae35b37e
Antonius Andar P. <andar.partogi@gmail.com>
2026-07-14 22:09:30 +0700

    security: stop logging local request bodies

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 
 // Request logging middleware
 app.use((req, res, next) => {
-  console.log(`[local-dev] ${req.method} ${req.url} - body:`, req.body);
+  console.log(`[local-dev] ${req.method} ${req.url}`);
   next();
 });
 
